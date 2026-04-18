483be9ea12bf95e6faaa31f9250c9b5a1867dc4a
shahan010100 <shahan010100@gmail.com>
2026-04-18 13:22:29 +0530

    add event listeners to render todo form and todos after creation, style todos display

--- src/createProjectform.js
+++ src/createProjectform.js
@@ -1,23 +1,30 @@
 import {projects, NewProject} from "./createProject.js"
 import {renderProjects} from "./ui.js"
+
 const divform=document.createElement('div');
 divform.classList.add('form', 'hidden');
+
 const projectForm=document.createElement('form');
 projectForm.classList.add('formA');
+
 const labelP=document.createElement('label');
 labelP.textContent='Project Name';
 labelP.setAttribute("for", "inputP");
 projectForm.append(labelP);
+
 const inputP=document.createElement('input');
 inputP.type='text';
 inputP.placeholder="Enter new project name";
 inputP.id="inputP";
 projectForm.append(inputP);
+
 const submitP=document.createElement('button');
 submitP.type='submit';
 submitP.textContent="Submit";
 projectForm.append(submitP);
+
 divform.append(projectForm);
+
 projectForm.addEventListener("submit", (e)=>{
  e.preventDefault();
  let tC=inputP.value;
--- src/ui.js
+++ src/ui.js
@@ -1,5 +1,8 @@
 import {projects} from "./createProject.js"
 import {divform} from "./createProjectform.js"
+import {divTDF} from "./createToDoForm.js"
+import { setCurrentProject } from "./createToDoForm.js";
+
 let body=document.querySelector('body');
 let content=document.createElement('div');
 let panel=document.createElement('div');
@@ -33,20 +36,93 @@ export function renderProjects() {
     allPro.innerHTML = "";
 
     projects.forEach(element => {
+        let divPC=document.createElement('div');
+        divPC.classList.add('divPC');
+        let divP=document.createElement('div');
+        divP.classList.add('divP');
+        let divPT=document.createElement('div');
         let pName = document.createElement('h5');
         pName.textContent = element.name;
-        pName.classList.add('pName');
+        pName.addEventListener('click', ()=>{
+        main.innerHTML='';
+        let mContainer=document.createElement('div');
+        mContainer.classList.add('mContainer');
+
+        let mPTitle=document.createElement('div');
+        mPTitle.classList.add('mPTitle');
+        mPTitle.textContent=element.name;
+
+        let mToDos=document.createElement('div');
+        mToDos.classList.add('mToDos');
+        element.todos.forEach(todo=>{
+            let title=todo.title;
+            let des=todo.description;
+            let dueD=todo.dueDate;
+            let priority=todo.priority;
+            let divC=document.createElement('div');
+            divC.classList.add('divC');
+            let divT=document.createElement('div');
+            divT.classList.add('divT');
+            divT.textContent=title;
+            let divD=document.createElement('div');
+            divD.classList.add('divD');
+            divD.textContent=des;
+            let divDD=document.createElement('div');
+            divDD.classList.add('divDD');
+            divDD.textContent=dueD;
+            let divP=document.createElement('div');
+            divP.textContent=priority;
+            if(priority=='High')
+            {
+                divC.classList.add('highP');
+            }
+            else if(priority=='Medium')
+            {
+                divC.classList.add('mediumP');
+            }
+            else{
+                divC.classList.add('lowP');
+            }
+            divC.append(divT, divD, divDD, divP);
+            mToDos.append(divC);
+
+        })
+
+        mContainer.append(mPTitle, mToDos);
+        main.appendChild(mContainer);}
+        )
+        pName.classList.add('pName', 'clickable');
+
+        let addTDB=document.createElement('button');
+        addTDB.type='button';
+        addTDB.name=element.name;
+        addTDB.textContent='+';
+        addTDB.classList.add('addTDB');
+        addTDB.addEventListener('click', ()=>{
+            main.innerHTML="";
+            setCurrentProject(element);
+            main.append(divTDF);           
+            divTDF.classList.remove('hidden');
+
+        });
+        
+
+        divP.append(pName, addTDB);
 
         element.todos.forEach(todo => {
             let todoP = document.createElement("h6");
             todoP.textContent = todo.title + " " + todo.dueDate;
-            todoP.classList.add('todoP');
-            pName.appendChild(todoP);
+            todoP.classList.add('todoP', 'clickable');
+            divPT.appendChild(todoP);
         });
+        divPC.append(divP, divPT);
 
-        allPro.appendChild(pName);
+        allPro.append(divPC);
     });
 }
 renderProjects();
 
 panel.append(pTitle,actionB,allPro);
+
+
+//main
